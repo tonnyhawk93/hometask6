5279e1b9a5d7194a32f57b81bec1871c6b0b63ed
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-24 23:49:10 +0300

    add alias to uuid

--- plugins/moduleLogger.ts
+++ plugins/moduleLogger.ts
@@ -1,4 +1,5 @@
 import { Compiler } from 'webpack';
+import fs from 'fs';
 
 class ModuleLogger {
     apply(compiler: Compiler) {
@@ -6,12 +7,7 @@ class ModuleLogger {
             'ModuleLogger',
             (normalModuleFactory) => {
                 normalModuleFactory.hooks.module.tap('ModuleLogger', (_module, _createData, resolveData) => {
-                    // @ts-ignore
-                    console.log('1 ' + _createData.resource);
-
-                    console.log('2 ' +resolveData.context);
-                    console.log('3 ' + module.paths);
-
+                    console.log(resolveData.context);
                     return _module;
                 });
             }
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,11 +2,11 @@ import * as path from 'path';
 import * as webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import StatoscopePlugin from '@statoscope/webpack-plugin';
-
 import ModuleLogger from './plugins/moduleLogger';
 
 const config: webpack.Configuration = {
     mode: 'production',
+    target: 'web',
     entry: {
         root: './src/pages/root.tsx',
         root2: './src/pages/root2.tsx',
@@ -15,11 +15,31 @@ const config: webpack.Configuration = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
     },
+    optimization: {
+        minimize: true,
+        moduleIds: 'deterministic',
+        innerGraph: true,
+        concatenateModules: true,
+        splitChunks: { 
+            chunks: "all",
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/    
+                }
+            }
+        }
+    },
+    cache: {
+        type: 'filesystem',
+        buildDependencies: {
+            config: [__filename]
+        }
+    },
     plugins: [
-        new HtmlWebpackPlugin({
-            title: 'hw6',
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html'
+        new HtmlWebpackPlugin({  // Also generate a test.html
+            filename: 'index.html',
+            template: 'src/index.html'
         }),
         new ModuleLogger(),
         new StatoscopePlugin({
@@ -34,12 +54,24 @@ const config: webpack.Configuration = {
             "buffer": require.resolve("buffer"),
             "stream": false,
         },
+        alias: {
+            '../components/Link' : path.resolve(__dirname, 'src/i.ts')
+        }
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/, 
+                include: path.resolve('src'),
                 use: [
+                    {
+                        loader: 'thread-loader', 
+                        options: {
+                            workers: 2,
+                            workerParallelJobs: 50,
+                            poolTimeout: 2000
+                        }
+                    },
                     {
                         loader: 'babel-loader', 
                         options: {
@@ -49,10 +81,11 @@ const config: webpack.Configuration = {
                     {
                         loader: 'ts-loader',
                         options: {
-                            transpileOnly: true
+                            transpileOnly: true,
+                            happyPackMode: true
                         }
                     }
-            ], 
+                    ], 
                 exclude: /node_modules/
             }
         ]
